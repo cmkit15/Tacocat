ed71efc016cd9460e7efff9c2d82767457f181c8
cmkit15 <cmkit15@gmail.com>
2020-10-10 20:57:04 -0400

    JS and prismjs update
    Added code snippet to codehtml and JS updates.

--- Tacocat/scripts/script.js
+++ Tacocat/scripts/script.js
@@ -1,9 +1,21 @@
-﻿// Function to reverse user input
-function reverse() {
+﻿// Event listener for submit button
+document.getElementById('submit').addEventListener('click', reverseString);
+
+// Event listener for enter key
+//document.getElementById("userInput").addEventListener("keypress", function (evt) {
+//    var character = (evt.which) ? evt.which : evt.keyCode;
+//    if (character >= 97 && character <= 122 || character >= 65 && character <= 90 || character == 13) {
+//        return true;
+//    } else {
+//        return false;
+//    }
+//});
+
+// Function to reverse user input
+function reverseString() {
     let userInput = document.getElementById('userInput').value;
     let userInputLower = userInput.toLowerCase();
     let palindrome = "";
-
     let errorMessage = "Your input cannot be blank!";
     if (userInput == "") {
         document.getElementById('tacocatResults').innerHTML = "";
@@ -12,7 +24,7 @@ function reverse() {
         document.getElementsByClassName('error-message')[0].innerHTML = "";
         for (let i = userInputLower.length - 1; i >= 0; i--) {
             palindrome = palindrome += userInputLower.charAt(i);
-            if (palindrome == userInput) {
+            if (palindrome == userInputLower) {
                 document.getElementById('tacocatResults').innerHTML = `${palindrome} <br /> is a palindrome`;
             } else {
                 document.getElementById('tacocatResults').innerHTML = `${palindrome} <br />Nope, do better next time!`;
@@ -27,15 +39,3 @@ function reset() {
     document.getElementById('userInput').value = "";
     document.getElementById('tacocatResults').innerHTML = "";
 }
-
-document.getElementById("userInput").addEventListener("keydown", function (evt) {
-
-    var character = (evt.which) ? evt.which : evt.keyCode;
-    if (character >= 97 && character <= 122 || character >= 65 && character <= 90 || character == 8) {
-        return true;
-
-    } else {
-        evt.preventDefault();
-        return false;
-    }
-});
